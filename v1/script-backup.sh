8dbddb504dd8fdedfa12c257106a274d98e1e66f
Hello, I am Mateus Eduardo :) <tequel_rp@hotmail.com>
2023-01-22 17:28:10 -0300

    Update script-backup.sh

--- v1/script-backup.sh
+++ v1/script-backup.sh
@@ -37,164 +37,115 @@ White="\e[0;97m"
   printf "${Green}\n Developed by: mmateuseduardo ( https://github.com/mmateuseduardo )"
   printf "${Green}\n Version: 1.0\n"
 
+##### MENU < TESTED OK
 menu () {
-	printf "\n${Yellow} [ Selecione uma opção para continuar ]\n\n"
-  printf " ${Red}[${Blue}1${Red}] ${Green}Instalar Dependencias - Curl - Wget\n"
-  printf " ${Red}[${Blue}2${Red}] ${Green}Informação os Dados do Telegram\n"
-  printf " ${Red}[${Blue}3${Red}] ${Green}Backup De Diretório\n"
-  printf " ${Red}[${Blue}4${Red}] ${Green}Backup Banco de Dados\n"
-  printf " ${Red}[${Blue}5${Red}] ${Green}Exit\n\n"
-  printf "${Green}┌─[${Red}Selecione uma Opção${Green}]──[${Red}~${Green}]─[${Yellow}Menu${Green}]:\n"
-  read -p "└──►$(tput setaf 7) " option
-  case $option in
-  1) Depedencia ;;
-	2) Telegram ;;
-	3) Diretorio ;;
-	4) DB ;;
-	5) Exit ;;
-  *) "Opcao desconhecida." ; echo ; Backup ;;
-
-    3) printf "\n[${Green}Selected${White}] Opção 3 - Backup De Diretório...\n\n"
-       apt install curl wget -y       
-       menu
-       ;;
+        printf "\n${Yellow} [ Selecione uma Opção para Continuar ]\n\n"
+        printf " ${Red}[${Blue}1${Red}] ${Green}Instalar Dependencias - Curl - Wget\n"
+        printf " ${Red}[${Blue}2${Red}] ${Green}Informar os Dados do Telegram\n"
+        printf " ${Red}[${Blue}3${Red}] ${Green}Backup De Diretório\n"
+        printf " ${Red}[${Blue}4${Red}] ${Green}Backup Banco de Dados\n"
+        printf " ${Red}[${Blue}5${Red}] ${Green}Exit\n\n"
+        printf "${Green}┌─[${Red}Selecione uma Opção${Green}]──[${Red}~${Green}]─[${Yellow}Menu${Green}]:\n"
+        read -p "└──►$(tput setaf 7) " option
+        case $option in
+        1) Depedencia ;;
+        2) Telegram ;;
+        3) Diretorio ;;
+        4) DB ;;
+        5) Exit ;;
+        *)printf "${White}[${Red}Error${White}] Selecione uma opção Correta...\n\n" ; menu ;;
 esac
 }
-Depedencia(){
-       printf "\n[${Green}Selected${White}] Opção 1 - Verifica Dependencias...\n\n"
-       apt install curl wget -y       
-       menu
-       ;;
-esac
-}
+##### Instalação de Depedencia < TESTED OK
+        Depedencia(){
+        printf "\n[${Green}Selected${White}] Opção 1 - Verificação de Dependencias...\n\n"
+        printf "${Red}\nInstalando Pacotes ${White}:)\n\n"
+        apt install curl wget zip -y
+        printf "${Red}\nPacotes Instalados ${White}:)\n\n"
+        menu
+        }
+##### Telegram < TESTED OK
 Telegram(){
-       printf "\n[${Green}Selected${White}] Opção 2 - Informação os Dados do Telegram...\n\n"
-       printf "${White}Informe o ID do Usuário/Grupo o TOKEN do BOOT e a Forma de Compactação TAR/ZIP "
-       printf "${White}User/Grupo ID: "
-       read ID     
-       printf "${White}TOKEN BOOT: "
-       read TOKEN
-       printf "${White}Forma de Compactação: "
-       read COMPAC
-       menu
-       ;;
-esac
+        printf "[${Green}Selected${White}] Opção 2 - Informação os Dados do Telegram...\n\n"
+        printf "\n${White}Informe o ID do Usuário/Grupo o TOKEN do BOOT e a Forma de Compactação TAR/ZIP\n"
+        printf "${White}User/Grupo ID: "
+        read ID
+        printf "${White}TOKEN BOOT: "
+        read TOKEN
+        printf "${White}Forma de Compactação: "
+        read COMPAC
+        printf "${Red}\nMENSAGEM DE TESTE ENVIADO PARA O USUÁRIO/GRUPO ${White}:)\n"
+        curl --silent -X POST \
+        --data-urlencode "chat_id=$ID" \
+        --data-urlencode "text=Enviando para USUÁRIO/GRUPO" \
+        "https://api.telegram.org/bot$TOKEN/sendMessage"
+        echo "\no\n"
+        printf "${Red}\nEfetuado Download e Configurado o Script para Envio de Mensagens ao Telegram ${White}:)\n"
+        wget https://raw.githubusercontent.com/remontti/telegramBotShell/master/telegram -O /bin/telegram
+        chmod +x /bin/telegram  /bin/telegram
+        sed -i "s/000000000:0000000000000-0000000000000000000000000000000/$TOKEN/" /bin/telegram
+        sed -i "s/tar/$COMPAC/" /bin/telegram
+        #   sed -i "s/22/$PORTA/"
+        printf "${Red}Arquivo salvo em /bin/telegram ${White}:)\n"
+        menu
 }
+##### BACKUP DIRETORIO1 < TESTED OK
 Diretorio(){
-       printf "\n[${Green}Selected${White}] Opção 3 - Backup De Diretório...\n\n"
-	     printf "\n${Yellow} [ Selecione uma opção para continuar ]\n\n"
-       printf " ${Red}[${Blue}1${Red}] ${Green}Backup de Um Diretório\n"
-       printf " ${Red}[${Blue}2${Red}] ${Green}Backup de Multiplos Diretório\n"
-       printf " ${Red}[${Blue}3${Red}] ${Green}Backup DNS Server\n"
-       printf " ${Red}[${Blue}4${Red}] ${Green}Backup FastNetmon\n"
-       printf " ${Red}[${Blue}5${Red}] ${Green}Backup SpeedTest/Minha Conexão\n"
-       printf " ${Red}[${Blue}6${Red}] ${Green}Voltar ao Menu Principal\n\n"
-       printf " ${Red}[${Blue}7${Red}] ${Green}Exit\n\n"
-       read opcao_var
-	     case $opcao_var in
-	     1) 1D ;;
-	     2) 2D ;;
-	     3) DNS ;;
-	     4) FastNetMon ;;
-	     5) SpeedTest ;;
-       6) Menu ;;
-       7) Exit ;;
-	     *) "Opcao desconhecida." ; echo ; Seta_Var ;;
+        printf "\n[${Green}Selected${White}] Opção 3 - Backup De Diretório...\n\n"
+        printf "\n${Yellow} [ Selecione uma opção para continuar ]\n\n"
+        printf " ${Red}[${Blue}1${Red}] ${Green}Backup de Diretórios\n"
+        printf " ${Red}[${Blue}2${Red}] ${Green}Backup DNS Server\n"
+        printf " ${Red}[${Blue}3${Red}] ${Green}Backup FastNetmon\n"
+        printf " ${Red}[${Blue}4${Red}] ${Green}Backup SpeedTest/Minha Conexão\n"
+        printf " ${Red}[${Blue}5${Red}] ${Green}Voltar ao Menu Principal\n"
+        printf " ${Red}[${Blue}6${Red}] ${Green}Exit\n\n"
+        printf "${Green}┌─[${Red}Selecione uma Opção${Green}]──[${Red}~${Green}]─[${Yellow}Menu${Green}]:\n"
+        read -p "└──►$(tput setaf 7) " opcao_var
+        case $opcao_var in
+        1) 1D ;;
+        2) DNS ;;
+        3) FastNetMon ;;
+        4) SpeedTest ;;
+        5) Menu ;;
+        6) Exit ;;
+        *)printf "${White}[${Red}Error${White}] Selecione uma opção Correta...\n\n" ; Diretorio ;;
 esac
 }
-		1D(){
-       printf "\n[${Green}Selected${White}] Option 1 Backup De UM Diretório...\n"
-       printf "${White}Informe o Diretorio para Backup, Informe o Caminho Completo Ex: /var/log/*"
-       printf "${White}Informe o Diretório: "
-       read Diretorio
-       printf "${White}Informe um nome para o Arquivo de Backup: "
-       read nomeD1
-       printf "${White}Informe um nome para Identificação "Comentário": "
-       read comentario
-       printf "${Red}Criado Diretório /backup/1D/ e Script ${Green}para armazenar as informações\n\n"
-       mkdir /backup/1D/
-       touch /backup/1D/1D.telegram.sh
-       chmod a+x  /backup/1D/1D.telegram.sh
-       ls -lah /backup/*
-       echo -e "#!/bin/bash\n/bin/telegram -f "$ID" "$Diretorio" "$nomeD1" "$comentario"
-       printf "${Red}Realizado um Backup de Teste, Verifique seu ${Blue}Telegram\n\n"
-       /bin/bash /backup/1D/1D.telegram.sh
-       menu
-				}
-     2D(){
-       printf "\n[${Green}Selected${White}] Option 2 Backup De Multiplos Diretório...\n"
-       printf "${White}Informe o Diretorio para Backup, Informe o Caminho Completo Ex: /var/log/*"
-       printf "${White}Informe o Diretório 1: "
-       read diretorio1
-       printf "${White}Informe um nome para o Arquivo de Backup: "
-       read nome1
-       printf "${White}Informe um nome para Identificação "Comentário": "
-       read comentario1
-       printf "${White}Informe o Diretório 2: "
-       read diretorio2
-       printf "${Red}Criado Diretório /backup/2D/ e Script ${Green}para armazenar as informações\n\n"
-       mkdir /backup/2D/
-       touch /backup/2D/2D.telegram.sh
-       chmod a+x  /backup/2D/2D.telegram.sh
-       ls -lah /backup/*
-       echo -e "#!/bin/bash\n/bin/telegram -f "$ID" "$diretorio1 $diretorio2" "nome1" "comentario1"
-       printf "${Red}Realizado um Backup de Teste, Verifique seu ${Blue}Telegram\n\n"
-       /bin/bash /backup/1D/1D.telegram.sh
-       menu
-				}
-		diretorio_captura(){
-		echo -n "Entre com o diretorio para salvar a coleta de pacotes "
-		read dirpacotes
-		if [ ! -d "$dirpacotes" ]; then 
-		    echo "Diretorio nao existe, criando diretorio..."
-		    echo "."
-		    echo ".."
-		    echo "..."
-		    mkdir $dirpacotes
-		    Seta_var
-		fi		## faltou fechar o if = (((dica: use a identação nos blocos )))
-		}		## Faltou fechar a chave da função  Diretorio captura
-		wordlist(){
-		echo -n "Entre com o caminho absoluto da Wordlist, ex /home/wordlist.txt "
-		read wordlist
-		if [ ! -e "$wordlist" ]; then
-		    echo "Wordlist nao existe ou foi digitada incorretamente"
-		    Seta_var	
-		fi		## faltou fechar o if = (((dica: use a identação nos blocos )))
-		}
-		parametros(){
-		echo "interface Wifi: $interface"
-		echo "Diretorio Captura: $dirpacotes"
-		echo "Worlist: $wordlist"
-		Seta_var		
-		}
-		Menu() {
-		menu	
-				}
-		Snnifer(){
-		airmon-ng start $interface
-		echo -n "Entre com o nome da interface de monitoramento, ex wlan0mon "
-		read mon_interface
-		airodump-ng $mon_interface
-		echo "Analise a rede wifi a ser quebrada e entre com os dados exatamente como aparecem"
-		echo "Digite o BSSID da rede desejada:"
-		read bssid
-		echo "Digite o Canal, campo CH:"
-		read canal
-		echo "Digite o client, mac do  pc que esta na rede"
-		read client
-		echo "Executando Snnifer.."
-		airodump-ng -c $canal --bssid $bssid -w $dirpacotes $mon_interface &
-		Wifi
-			}
-		Inject(){
-		aireplay-ng -0 2 $bssid -c $client $mon_interface
-		sleep 10
-		Wifi 
-			}
-		Crack(){
-		aircrack-ng -a2 -b $bssid -w $wordlist $dirpacotes/*.cap
-		sleep 10
-		Wifi
-			}
-Wifi 
+        1D(){
+        printf "\n[${Green}Selected${White}] Option 1 Backup De Diretório...\n"
+        printf "${White}Informe o Diretorio para Backup, insira o Caminho Completo Ex: /var/log/*"
+        printf "\n${Red}Criado Diretório /backup/D/ e o Script ${Green}para armazenar as informações\n\n"
+        mkdir -p /backup/d/
+        touch /backup/d/d.telegram.sh
+        chmod a+x  /backup/d/d.telegram.sh
+        ls -lah /backup/*
+        echo "!/bin/bash" > diretoriotexto.txt
+        printf "\n${Blue}Informe a quantidade de  Diretório: "
+        read dir
+        x=1
+        while [ $x -le $dir ]
+        do
+        printf "${White}Insira o Diretorio $x: "
+        read idir
+        printf "${White}Informe um nome para o Arquivo de Backup: "
+        read nome
+        printf "${White}Informe um nome para Identificação "Comentário": "
+        read comentario
+        printf "\n${Red}==================================================================================\n"
+        echo "/bin/telegram -f \""$ID"\" \""$idir"\" \""$nome"\" \""$comentario"\"" >> /backup/d/d.telegram.sh
+        x=$(( $x + 1 ))
+done
+        #printf "\n${White}POR PADRÃO ESSE ESCRIPT RODA UMA VEZ POR SEMANA, DESEJA EFETUAR ALTERAÇÃO ???"
+        printf "\n${RED}ESSA ALTERAÇÃO PODE SER EFETUADA NO ARQUIVO /var/spool/cron/crontabs/root..."
+        grep -q /backup/d/d.telegram.sh /var/spool/cron/crontabs/root || echo "10 10 * 1 * /backup/d/d.telegram.sh " >> /var/spool/cron/crontabs/root
+        cat /backup/d/d.telegram.sh
+        printf "\n${Blue}PARA TESTE FOI ENVIADO UM BACKUP AO SEU TELEGRAM, POR FAVOR VERIFIQUE\n"
+        menu
+}
+##### Exit < TESTED OK
+        Exit (){
+        printf "${Red}\nObrigado por usar o Scirpt ${White}:)\n\n"
+        exit 0
+}
+
+menu
